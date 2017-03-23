c752fe3eb38c6958de3ea212db921e140ff98270
Israel Nunez <israeln@sandata.com>
2017-03-23 15:38:12 -0400

    add all tables required for assignment 2

--- Module3-create-scripts.sql
+++ Module3-create-scripts.sql
@@ -11,7 +11,7 @@ Zip varchar2(10) not null,
 
 constraint CustNo_PK primary key (CustNo)
 );
-create table Employee(
+create table employee(
 EmpNo varchar2(8) not null,
 EmpName varchar2(30) not null.
 Department varchar2(20) not null,
@@ -35,4 +35,39 @@ constraint unique_email unique (Email)
  
  constraint LocNo_PK primary key (LocNo),
  constraint FacNo_FK foreign key (FacNo) references facility (FacNo)
+ );
+ create table resourcetbl(
+ ResNo varchar2(8) not null,
+ ResName varchar2(30) not null,
+ Rate number(2,2)
+ 
+ constraint Resno_PK primary key (ResNo),
+ );
+ create table eventrequest(
+ EventNo varchar2(8) not null,
+ DatehHeld date not null,
+ DateReq date not null,
+ FacNo varchar2(8) not null,
+ CustNo varchar2(8) not null,
+ dateauth date,
+ status varchar2(15) not null,
+ EstCost number(4,2) not null,
+ EstAudience number(5,0) not null,
+ BudNo varchar2(8),
+ 
+ constraint EventNo_PK primary key (EventNo),
+ constraint FacNo_FK foreign key references facility (FacNo),
+ constraint CustNo_FK foreign key references customer (CustNo)
+ );
+ create table eventplan(
+ PlanNo varchar2(8) not null,
+ EventNo varchar2(8) not null,
+ WorkDate date not null,
+ Notes varchar2(30),
+ Activity varchar2(20) not null,
+ EmpNo varchar2(8),
+ 
+ constraint PlanNo_PK primary key (PlanNo),
+ constraint EventNo_FK foreign key references eventrequest (EventNo),
+ constraint EmpNo_FK foreign key references employee (EmpNo)
  );
